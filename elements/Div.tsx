77731f519338faaf231c1ed712ca8d61b7d01f5f
Saba Phruidze <sabaphruidze01@gmail.com>
2024-04-20 17:22:55 +0400

    I added styled components which is best css library for me and now I will add navigation

---
+++ elements/Div.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styled from 'styled-components/native';
+
+export const Div = styled.View`
+  flex: 1;
+  background-color: #b0e0e6;
+  align-items: 'center';
+  justify-content: 'center';
+`;

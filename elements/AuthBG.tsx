255bd0441f91375ed055ea3407b8103f1955b78c
Saba Phruidze <sabaphruidze01@gmail.com>
2024-04-28 16:55:11 +0400

    I used photoshop in order to make dark and light images for login, register, auth and forgot password and know I am going to add 2 icon chat will change the colors using useReducer

--- elements/AuthBG.tsx
+++ elements/AuthBG.tsx
@@ -6,4 +6,6 @@ export const AuthBG = styled.ImageBackground`
   flex: 1;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 100%;
 `;
--- navigation/Authentication.tsx
+++ navigation/Authentication.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import {AuthBG} from '../elements/AuthBG';
-
 import {AuthView} from '../elements/AuthView';
 import {AuthButtonLogin} from '../navigationChild/AuthButton';
 import {AuthButtonRegister} from '../navigationChild/AuthButton';
 
 const Authentication = ({navigation}: {navigation: any}) => {
   return (
-    <AuthBG source={require('../assets/images/Authentication.png')}>
+    <AuthBG
+      source={require('../assets/images/Bright/Authentication.png')}
+      resizeMode="cover">
       <AuthView>
         <AuthButtonLogin navigation={navigation} />
         <AuthButtonRegister navigation={navigation} />

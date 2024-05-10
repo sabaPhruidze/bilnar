14d6b57a192b9907c6f3ecb665f7f43ec0069d90
Saba Phruidze <sabaphruidze01@gmail.com>
2024-05-10 22:34:59 +0400

    Currently trying to receive current screen information and with that change the authStack show or not the header

--- drawer/Start.tsx
+++ drawer/Start.tsx
@@ -2,17 +2,26 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import Authentication from '../navigation/Authentication';
 import MainStack from './MainStack';
 import AuthStack from './AuthStack';
-
+import {useNavigation} from '@react-navigation/native';
 const Drawer = createDrawerNavigator();
 
 export default function MyDrawer() {
+  const navigation = useNavigation();
+  const currentScreen = navigation.getCurrentRoute().name;
+  console.log(currentScreen);
+
   return (
     <Drawer.Navigator initialRouteName="HomeStack">
       <Drawer.Screen
         name="AuthStack"
         component={AuthStack}
         options={{
-          headerShown: false,
+          headerShown:
+            currentScreen === 'Authentication' ||
+            currentScreen === 'Login' ||
+            currentScreen === 'Register'
+              ? false
+              : true,
         }}
       />
       <Drawer.Screen

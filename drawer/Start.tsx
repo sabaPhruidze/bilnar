42c170c408c8463171bf05a107eaa92e70c4c5ef
Saba Phruidze <sabaphruidze01@gmail.com>
2024-05-10 23:21:02 +0400

    Yei I solved it through adding eventlistener

--- drawer/Start.tsx
+++ drawer/Start.tsx
@@ -3,16 +3,34 @@ import {useEffect, useRef, useState} from 'react';
 import MainStack from './MainStack';
 import AuthStack from './AuthStack';
 import {useNavigation} from '@react-navigation/native';
+
 const Drawer = createDrawerNavigator();
 
 export default function MyDrawer() {
+  const navigation = useNavigation();
+  const [currentScreen, setCurrentScreen] = useState('');
+  const ALR =
+    currentScreen === 'Authentication' ||
+    currentScreen === 'Login' ||
+    currentScreen === 'Register';
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('state', () => {
+      const currentRouteName = navigation.getCurrentRoute().name;
+      setCurrentScreen(currentRouteName);
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  console.log('Current Screen:', currentScreen); // You can log or use the current screen name here
+
   return (
     <Drawer.Navigator initialRouteName="HomeStack">
       <Drawer.Screen
         name="AuthStack"
         component={AuthStack}
         options={{
-          headerShown: false,
+          headerShown: ALR ? false : true,
         }}
       />
       <Drawer.Screen

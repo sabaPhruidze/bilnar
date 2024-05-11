bbb2dce527ffeb91daecfc4fa5d5261a17faf842
Saba Phruidze <sabaphruidze01@gmail.com>
2024-05-11 23:32:13 +0400

    I added the TaskCard in the Main page and currently I am thinking about what would if it would be better to make a database of firebase and than when someone decides to add a task he will send this information in the firebase and then the information will be send back to the app in order to be shown

--- navigation/Main.tsx
+++ navigation/Main.tsx
@@ -4,6 +4,7 @@ import {AuthBGView} from '../elements/AuthBGView';
 import SwitchForBG from '../navigationChild/SwitchForBG';
 import React, {useContext} from 'react';
 import {myContext} from '../App';
+import MainTaskCard from '../navigationChild/MainTaskCard';
 
 const Main = () => {
   const MainContext = useContext(myContext);
@@ -12,6 +13,7 @@ const Main = () => {
   return (
     <AuthBGView style={{backgroundColor: switchBG ? 'black' : 'white'}}>
       <SwitchForBG />
+      <MainTaskCard />
     </AuthBGView>
   );
 };
